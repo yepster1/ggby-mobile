63657ae14e81ac7131eb8b7b87cbe3aa1636e666
Alex Vargas <vlexaargas@gmail.com>
2018-09-16 22:20:26 -0400

    Add header and placeholder for GGBY logo.

--- src/features/rootNavigator/index.js
+++ src/features/rootNavigator/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-import { createBottomTabNavigator } from "react-navigation";
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from "react-navigation";
+
 import { View, Text } from "react-native";
 import Info from "features/info";
 
-const RootNavigator = createBottomTabNavigator({
+// NOTE: If the RootNavigator get any more complex (i.e.  accounts/registration
+// features) we'll probably want to move this into its own feature dir. For
+// now, the RootNavigator below is pretty much just providing a header.
+
+const ApplicationNavigator = createBottomTabNavigator({
   Schedule: () => (
     <View>
       <Text>Schedule</Text>
@@ -17,5 +25,18 @@ const RootNavigator = createBottomTabNavigator({
   ),
   Info
 });
+
+const RootNavigator = createStackNavigator(
+  {
+    Application: {
+      screen: ApplicationNavigator
+    }
+  },
+  {
+    navigationOptions: {
+      title: "GGBY 2018"
+    }
+  }
+);
 
 export default RootNavigator;
