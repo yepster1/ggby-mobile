fca9d7f21f052622c0ade7bb30a1de4b7eabe49c
Guillaume Labranche <gumgl@users.noreply.github.com>
2018-10-11 23:41:52 -0700

    Implement MapNavigator

--- src/features/map/MapNavigator.js
+++ src/features/map/MapNavigator.js
@@ -4,10 +4,33 @@ import { View, Text } from "react-native";
 import { createStackNavigator } from "react-navigation";
 import defaultStackNavigatorConfigs from "components/navigator";
 
+import { MapView } from 'expo';
+import { Overlay } from 'react-native-maps';
+import fruitbowl from '../assets/images/fruit-bowl.png';
+
+this.state = {
+  overlay_bounds: [[47.643854,-122.308841],[47.640853,-122.303558]],
+};
+
+Expo.Permissions.getAsync(Expo.Permissions.LOCATION);
+
 const Map = () => (
-  <View>
-    <Text>Map goes here</Text>
-  </View>
+  <MapView
+    style={{ flex: 1 }}
+    region={{
+      latitude: 47.642234,
+      longitude: -122.305725,
+      latitudeDelta: 0.004,
+      longitudeDelta: 0.005,
+    }}
+    showsUserLocation={true}
+    showsMyLocationButton={true}
+  >
+  <Overlay
+    image={fruitbowl}
+    bounds={this.state.overlay_bounds}
+  />
+  </MapView>
 );
 
 const MapNavigator = createStackNavigator(
