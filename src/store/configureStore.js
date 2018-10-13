7815b9f2c626860e578b59850d0ec9fe2b5d1523
Alex Vargas <vlexaargas@gmail.com>
2018-10-13 16:31:32 -0400

    Get event reminders persisted to device.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,8 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import { createEpicMiddleware } from "redux-observable";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import rootReducer from "domain";
 import rootEpic from "epics";
 
@@ -7,14 +10,24 @@ const epicMiddleware = createEpicMiddleware();
 // eslint-disable-next-line no-underscore-dangle
 const composeDevtool = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["eventReminders"]
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export default function configureStore() {
   const store = createStore(
-    rootReducer,
+    persistedReducer,
     composeDevtool(applyMiddleware(epicMiddleware))
   );
+
+  const persistor = persistStore(store);
 
   epicMiddleware.run(rootEpic);
   store.dispatch({ type: "INIT_APP" });
 
-  return store;
+  return { store, persistor };
 }
